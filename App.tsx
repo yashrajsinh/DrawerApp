79ea0102619783e801fd97a42b2901d635785ae0
yash <yashrajsinhjadeja2253@gmail.com>
2026-03-09 14:06:17 +0530

    Completed with right structure
    Task completed with right folder structure

--- App.tsx
+++ App.tsx
@@ -2,37 +2,16 @@ import 'react-native-gesture-handler';
 import React from 'react';
 //Nav Container
 import { NavigationContainer } from '@react-navigation/native';
-//Drawer
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import DrawerNavigator from './src/navigation/DrawerNavigator';
 
-//Screens
-import HomeScreen from './src/screens/HomeScreen';
-import ProfileScreen from './src/screens/ProfileScreen';
-import SearchScreen from './src/screens/SearchScreen';
-import { Text } from 'react-native';
-
-const Drawer = createDrawerNavigator();
+//Drawer Compnent
+DrawerNavigator;
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
-        screenOptions={({ navigation }) => ({
-          drawerType: 'slide',
-          headerLeft: () => (
-            <Text
-              style={{ fontSize: 24, marginLeft: 15 }}
-              onPress={() => navigation.toggleDrawer()}
-            >
-              🟰
-            </Text>
-          ),
-        })}
-      >
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Search" component={SearchScreen} />
-        <Drawer.Screen name="Profile" component={ProfileScreen} />
-      </Drawer.Navigator>
+      {/* === Custom Drawer Component ==== */}
+      <DrawerNavigator />
     </NavigationContainer>
   );
 }
