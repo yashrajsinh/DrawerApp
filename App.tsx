f91d7f013f0a0b48da8c6162984295068780ac1b
yash <yashrajsinhjadeja2253@gmail.com>
2026-03-09 13:44:03 +0530

    Initial Commit
    Drawer works fine

--- App.tsx
+++ App.tsx
@@ -1,45 +1,39 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import 'react-native-gesture-handler';
+import React from 'react';
+//Nav Container
+import { NavigationContainer } from '@react-navigation/native';
+//Drawer
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+//Screens
+import HomeScreen from './src/screens/HomeScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+import SearchScreen from './src/screens/SearchScreen';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+//Custom Icon
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
+const Drawer = createDrawerNavigator();
+
+export default function App() {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <Drawer.Navigator
+        screenOptions={({ navigation }) => ({
+          headerLeft: () => (
+            <Icon
+              name="menu"
+              size={28}
+              style={{ marginLeft: 15 }}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          ),
+        })}
+      >
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Search" component={SearchScreen} />
+        <Drawer.Screen name="Profile" component={ProfileScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
